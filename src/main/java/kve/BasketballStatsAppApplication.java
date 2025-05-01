f5552b061a1162dcd1e11ab899f782308064ec26
koralv <koralv@e-bitbox.com>
2025-05-01 20:10:55 +0300

    Initial commit

---
+++ src/main/java/kve/BasketballStatsAppApplication.java
@@ -0,0 +1,13 @@
+package kve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballStatsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasketballStatsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kve/basketballstatsapp/BasketballStatsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kve.basketballstatsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballStatsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
